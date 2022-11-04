3560464d33104c5a241450b0522dc9b38bbc890a
Songhan Yu <songhanysh@gmail.com>
2022-11-04 13:07:45 -0400

    udpate

---
+++ test/nov4.erl
@@ -0,0 +1,13 @@
+-module(ex).
+-compile(export_all).
+
+q1() ->
+    queue:in(1, queue:in(2, queue:in(3, queue:new()))).
+
+sizeq(Q) ->
+    case queue:out(Q) of 
+      {empty, _} ->
+          0;
+      {{value, _}, Rest} ->
+          1 + sizeq(Rest)
+    end.
